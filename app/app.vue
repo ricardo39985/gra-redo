2a53b490c2c8c4ea9f31491fd627a244fb2950a1
Ricky <ricardo39985@gmail.com>
2025-09-06 12:44:50 -0400

    init

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="bg-red-500">
+        <v-btn color="primary">
+          Hello Vuetify!
+        </v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
